d96bff9684a3ff6c01e3d8dc184734559044284f
Tran Huu Nhan <huunhantran10@gmail.com>
2024-01-11 17:36:01 +0700

    deploye demo

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+app.use(express.static('public'));
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'demo',
+app.get('/about', (req, res) => {
+    let a = 1,
+        b = 2;
+    return res.send(`a + b = ${a + b}`);
 });
 
-conn.connect();
-
-conn.query('SELECT * FROM users', function (err, rows, fields) {
-    if (err) throw err;
-    console.log(rows);
+app.get('/users', (req, res) => {
+    const data = fs.readFileSync('./data.txt', 'utf8');
+    return res.send(data);
 });
 
-conn.end();
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
